bf9d073980e6a2cbb0ecaf562e3d2935a4865ca6
asrarmanaru-create <asrarmanaru@gmail.com>
2025-10-23 13:27:06 +0200

    as

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     <div class="seksjon">
         <h2>Klasse</h2>
         <ul>
-            <li><a href="klasse/registrer_klasse.php">Registrer klasse</a></li>
+            <a href="klasse/legg_til_klasse.php">Registrer klasse</a>            
             <li><a href="klasse/vis_klasse.php">Vis alle klasser</a></li>
             <li><a href="klasse/slett_klasse.php">Slett klasse</a></li>
         </ul>
@@ -38,7 +38,7 @@
     <div class="seksjon">
         <h2>Student</h2>
         <ul>
-            <li><a href="student/registrer_student.php">Registrer student</a></li>
+            <li><a href="student/legg_til_student.php">Registrer student</a></li>
             <li><a href="student/vis_student.php">Vis alle studenter</a></li>
             <li><a href="student/slett_student.php">Slett student</a></li>
         </ul>
