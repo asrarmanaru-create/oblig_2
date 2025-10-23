e754b5e5e1ab644060f2a0e5cbc91b6402cf98fa
asrarmanaru-create <asrarmanaru@gmail.com>
2025-10-23 11:09:48 +0200

    s

--- db.php
+++ db.php
@@ -1,13 +1,15 @@
 <?php
-$host = "b-studentsql-1.usn.no";   // USN database-server
-$dbname = "asaln7138";             // Ditt databasenavn (samme som brukernavn)
-$user = "asaln7138";               // Din databasebruker
-$pass = "1251asaln7138";            // Passordet du bruker til phpMyAdmin
+$host = "b-studentsql-1.usn.no";  
+$dbname = "asaln7138";            
+$user = "asaln7138";              
+$pass = "1251asaln7138";           
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+    // echo "Tilkoblet databasen!"; // valgfritt for testing
+} catch (PDOException $e) {
     echo "Databasekobling feilet: " . $e->getMessage();
 }
 ?>
+
